6f324d3469dce095430523b80768a7b6da822057
libaichuan <libaichuan@nibirutech.com>
2017-08-07 19:27:24 +0800

    测试docker

--- src/wbwgo/main.go
+++ src/wbwgo/main.go
@@ -2,34 +2,17 @@ package main
 
 import (
 	"fmt"
-	//"reflect"
+	"time"
 )
 
-//"time"
-//"wbwgo/game"
-
-type AA interface {
-	say()
-	sayno()
-}
-type BB interface {
-	say()
-}
-
-type Person struct {
-}
-
-func (p *Person) say() {
-	fmt.Printf("hello")
-}
-
-func (p *Person) sayno() {
-	fmt.Printf("no")
-}
-
-var b AA
-
 func main() {
-	b = &Person{}
-	b.say()
+	ticker := time.NewTicker(time.Second)
+
+	for {
+		select {
+		case <-ticker.C:
+			fmt.Println("hello")
+			break
+		}
+	}
 }
